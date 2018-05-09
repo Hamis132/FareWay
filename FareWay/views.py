a320f2a75be84018b19d9343f38670b29cdce2f2
LubieKaszke <martyna.kolakowska94@gmail.com>
2018-05-09 13:01:04 +0200

    Template "skonczone"

--- FareWay/views.py
+++ FareWay/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("This is Home View.")
+    return render(request,'home.html', {})
--- FareWayProj/urls.py
+++ FareWayProj/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import include
+from django.conf.urls import url
 
 urlpatterns = [
-    path('', include('FareWay.urls')),
-    """path('admin/', admin.site.urls),"""
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('FareWay.urls')),
+
 ]
