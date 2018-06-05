a107e27aa0f338f68cd70fdc048ad7fc04ba7543
Michał Rutkowski <Fosternmr@gmail.com>
2018-06-05 19:41:49 +0200

    Poprawki v2

--- FareWay/views.py
+++ FareWay/views.py
@@ -6,14 +6,9 @@ from django.shortcuts import render, get_object_or_404
 
 def home(request):
     attractions = Attraction.objects.get_queryset()
-<<<<<<< HEAD
-    return render(request, 'home.html', {'attractions': attractions})
-
-
-=======
     return render(request,'home.html', {'attractions':attractions})
 
 def attraction_details(request,pk):
     attraction=get_object_or_404(Attraction,pk=pk)
     return render(request,'attraction_details.html',{'attraction':attraction})
->>>>>>> a59c0d55e9c97e97198e45c992173a7003629899
+# a59c0d55e9c97e97198e45c992173a7003629899
