9f8e17d8a10cd7fee9730c7741fa3a31652deda0
Michał Rutkowski <Fosternmr@gmail.com>
2018-06-05 19:22:01 +0200

    poczatek login

--- FareWay/views.py
+++ FareWay/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Attraction
 
+
 def home(request):
     attractions = Attraction.objects.get_queryset()
-    return render(request,'home.html', {'attractions':attractions})
+    return render(request, 'home.html', {'attractions': attractions})
+
+
--- FareWayProj/urls.py
+++ FareWayProj/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include
 from django.conf.urls import include, url
 from django.conf import settings
 from django.views.static import serve
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^registration/login', views.login, {'template_name': 'registration/login.html'}, name='login'),
     url(r'', include('FareWay.urls')),
 
 ]
