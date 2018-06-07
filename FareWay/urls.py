9672b9c1e6b2404d2c90d6c2d4ac3af85f78b17d
LubieKaszke <martyna.kolakowska94@gmail.com>
2018-06-07 17:44:02 +0200

    nieskończona rejestracja

--- FareWay/urls.py
+++ FareWay/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^registration/register/$', views.register, name='register'),
+    url(r'^registration/register/success/$', views.registration_complete, name='registration_complete'),
     url(r'^attraction/(?P<pk>\d+)/$', views.attraction_details, name='attraction_details'),
 ]
--- FareWay/views.py
+++ FareWay/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+
+from django.contrib.auth.forms import UserCreationForm
 from .models import Attraction
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 
 def home(request):
@@ -12,3 +12,17 @@ def attraction_details(request,pk):
     attraction=get_object_or_404(Attraction,pk=pk)
     return render(request,'attraction_details.html',{'attraction':attraction})
 # a59c0d55e9c97e97198e45c992173a7003629899
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/register/success')
+    else:
+        form = UserCreationForm()
+        args ={'form':form}
+    return render(request, "registration/register.html", args)
+
+def registration_complete(request):
+    return render(request, 'registration/registration_complete.html')
